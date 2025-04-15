8b1dba47611ff7771297be5d25850675cde1cc27
ishchukserhii <ischuksergei@gmail.com>
2025-04-15 23:43:30 +0300

    4

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -1,16 +1,46 @@
-import React from 'react'
-import css from './ContactForm.module.css'
+import React from "react";
+import css from "./ContactForm.module.css";
+import { Formik, Form, Field, ErrorMessage } from "formik";
+import * as Yup from "yup";
+
+const UserSchema = Yup.object().shape({
+  name: Yup.string()
+    .min(3, "Must be min 3 chars")
+    .max(50, "Max 50 chars")
+    .matches(
+      /^[A-Za-zА-Яа-яЇїІіЄєҐґ'’`\- ]+$/,
+      "Name must contain only letters and spaces"
+    )
+    .required("This field is required"),
+  number: Yup.string()
+    .required("This field is required")
+    .matches(/^\d+$/, "Must be a valid number")
+    .min(3, "Must be at least 3 digits")
+    .max(50, "Must be at most 50 digits"),
+});
 
 const ContactForm = () => {
   return (
-    <form className={css.box}>
+    <Formik
+      initialValues={{
+        id: "",
+        name: "",
+        number: "",
+      }}
+      validationSchema={UserSchema}
+      // onSubmit={handleSubmit}
+    >
+      <Form className={css.box}>
         <label htmlFor="Name">Name</label>
-        <input type="text" />
+        <Field type="text" name="name" />
+        <ErrorMessage name="name" component="span" />
         <label htmlFor="Number">Number</label>
-        <input type="text" />
-        <button>Add contact</button>
-    </form>
-  )
-}
+        <Field type="tel" name="number" />
+        <ErrorMessage name="number" component="span" />
+        <button className={css.buttom}>Add contact</button>
+      </Form>
+    </Formik>
+  );
+};
 
-export default ContactForm
+export default ContactForm;
--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -1,17 +1,16 @@
-import React from 'react'
-import Contact from '../Contact/Contact';
-
-const ContactList = ({Filter}) => {
-
+import React from "react";
+import Contact from "../Contact/Contact";
+import css from "./ContactList.module.css";
+const ContactList = ({ Filter }) => {
   return (
-    <ul>
-      {Filter.map(contact => (
+    <ul className={css.list}>
+      {Filter.map((contact) => (
         <li key={contact.id}>
-          <Contact name={contact.name} phone={contact.number}/>
+          <Contact name={contact.name} phone={contact.number} />
         </li>
       ))}
     </ul>
   );
 };
 
-export default ContactList
+export default ContactList;
